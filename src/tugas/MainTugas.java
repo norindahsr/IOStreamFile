7033c74fee0f3c31170c6bae0599498e201b7403
norindahsr <2410010427indah@gmail.com>
2026-06-19 12:56:07 +0800

    saya sudah menyelesaikan main tugas java

--- src/tugas/MainTugas.java
+++ src/tugas/MainTugas.java
@@ -7,7 +7,7 @@ public class MainTugas {
 
     public static void main(String[] args) {
 
-        // Array kategori (Minimal 3) [cite: 341]
+        // Array kategori
         String[] kategori = {
             "Makanan",
             "Minuman",
@@ -15,32 +15,37 @@ public class MainTugas {
         };
 
         System.out.println("=== KATEGORI BARANG ===");
+
         for (String k : kategori) {
             System.out.println("- " + k);
         }
+
         System.out.println();
 
-        // Membuat objek gudang pertama [cite: 339]
+        // Membuat gudang utama
         Gudang gudang = new Gudang("barang.txt");
 
-        // Menambah minimal 5 objek Barang [cite: 339]
+        // Menambah 5 barang sesuai studi kasus
         gudang.tambahBarang(new Barang("Pensil", 3000, 20));
         gudang.tambahBarang(new Barang("Buku", 12000, 15));
         gudang.tambahBarang(new Barang("Penghapus", 2000, 10));
         gudang.tambahBarang(new Barang("Air Mineral", 5000, 25));
         gudang.tambahBarang(new Barang("Roti", 8000, 12));
 
-        // Menyimpan seluruh data barang awal ke berkas teks [cite: 342]
+        // Menyimpan seluruh data barang awal ke berkas teks
         gudang.simpanKeBerkas();
         System.out.println();
 
-        // Membuat objek Gudang baru yang kosong untuk membuktikan data tersimpan [cite: 343]
+        // Membuat objek Gudang baru yang kosong untuk membuktikan data berhasil di-load dari file
         System.out.println("=== Memuat Ulang Data pada Gudang Baru ===");
         Gudang gudangBaru = new Gudang("barang.txt");
         gudangBaru.muatDariBerkas();
         
-        // Menampilkan data hasil muat ulang dan menghitung total nilai persediaan [cite: 343]
+        // Menampilkan data hasil muat ulang
         gudangBaru.tampilkanSemua();
-        System.out.println("Total Nilai Persediaan : Rp" + gudangBaru.totalNilai());
+        
+        // Menampilkan total nilai persediaan aset (Harga x Stok)
+        System.out.println();
+        System.out.println("Total Nilai Persediaan Aset: Rp" + gudangBaru.totalNilai());
     }
 }
